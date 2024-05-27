fb4d6a7f86b451f32bfb80fab93a3a2b9ddbfdd4
petersiMicrostepMis <138105553+petersiMicrostepMis@users.noreply.github.com>
2024-05-27 17:00:49 +0200

    Update test_unit_model.py

--- ai4eosc_thunder_nowcast_ml/tests/test_unit_model.py
+++ ai4eosc_thunder_nowcast_ml/tests/test_unit_model.py
@@ -7,23 +7,22 @@ class TestModelMethods(unittest.TestCase):
     def setUp(self):
         self.meta = api.get_metadata()
 
-    def test_model_metadata_type(self):
-        """
-        Test that get_metadata() returns dict
-        """
-        # self.assertTrue(type(self.meta) is dict)
+    # def test_model_metadata_type(self):
+    #     """
+    #     Test that get_metadata() returns dict
+    #     """
+    #     self.assertTrue(type(self.meta) is dict)
 
-    def test_model_metadata_values(self):
-        """
-        Test that get_metadata() returns right values (subset)
-        """
-        # self.assertEqual(self.meta['name'].lower().replace('-','_'),
-        #                 'ai4eosc_thunder_nowcast_ml'.lower().replace('-','_'))
-        # self.assertEqual(self.meta['author'].lower(),
-        #                  'MicroStep-MIS'.lower())
-        # self.assertEqual(self.meta['license'].lower(),
-        #                  'MIT'.lower())
-
+    # def test_model_metadata_values(self):
+    #     """
+    #     Test that get_metadata() returns right values (subset)
+    #     """
+    #     self.assertEqual(self.meta['name'].lower().replace('-','_'),
+    #                      'ai4eosc_thunder_nowcast_ml'.lower().replace('-','_'))
+    #     self.assertEqual(self.meta['author'].lower(),
+    #                      'MicroStep-MIS'.lower())
+    #     self.assertEqual(self.meta['license'].lower(),
+    #                      'MIT'.lower())
 
 if __name__ == '__main__':
     unittest.main()
