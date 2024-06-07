f36e13400948068416aaa8589b620477f5518856
petersiMicrostepMis <138105553+petersiMicrostepMis@users.noreply.github.com>
2024-06-07 16:56:02 +0200

    Update config_layout.py

--- ai4eosc_thunder_nowcast_ml/config_layout.py
+++ ai4eosc_thunder_nowcast_ml/config_layout.py
@@ -1,7 +1,7 @@
 NAME = "ai4eosc_thunder_nowcast_ml"
 
 # working directory
-BASE_DIR = "/srv/thunderstorm-nowcast-microstep"
+BASE_DIR = "/storage/thunderstorm-nowcast-microstep"  # "/srv/thunderstorm-nowcast-microstep"
 WORKING_DATA_DIR = BASE_DIR + "/" + NAME + "/dataset/data_working_directory"
 RAW_DATA_DIR = WORKING_DATA_DIR + "/raw"
 TRAIN_DIR = WORKING_DATA_DIR + "/train"
